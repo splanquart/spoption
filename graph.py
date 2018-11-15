44742f50090b8ece4bf5e600831baba56541a861
Stéphane Planquart <stephane@planquart.com>
2018-11-15 18:43:02 +0100

    rainbow graph

--- graph.py
+++ graph.py
@@ -1,34 +1,46 @@
+from math import ceil
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import HTML, display
 import tabulate
 
+from matplotlib import colors as mcolors
+from IPython.display import HTML, display
 
+
+def rainbow_color(size):
+    """Return a color list of size element lire rainbow
+    """
+    colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
+    by_hsv = sorted((tuple(mcolors.rgb_to_hsv(mcolors.to_rgba(color)[:3])), name)
+                    for name, color in colors.items())
+    sorted_names = [name for hsv, name in by_hsv]
+    modulo = ceil(len(sorted_names) / size)
+    return sorted_names[::modulo]
 
 def show_profit(x, y, label, color):    
     fig, ax = plt.subplots()
     plt.style.use('dark_background')
-    ax.spines['top'].set_visible(False) # Top border removed
-    ax.spines['right'].set_visible(False) # Right border removed
-    ax.spines['bottom'].set_position('zero') # Sets the X-axis in the center
+    ax.spines['top'].set_visible(False)  # Top border removed
+    ax.spines['right'].set_visible(False)  # Right border removed
+    ax.spines['bottom'].set_position('zero')  # Sets the X-axis in the center
     ax.plot(x, y, label=label,color=color)
     plt.xlabel('Stock Price')
     plt.ylabel('Profit and loss')
-    plt.legend()
+    plt.legend(loc='best')
     # plt.title('Profit Scheme')
     plt.show()
 def show_profit_compare(x, y1, label1, color1, y2, label2, color2):    
     fig, ax = plt.subplots()
     plt.style.use('dark_background')
-    ax.spines['top'].set_visible(False) # Top border removed
-    ax.spines['right'].set_visible(False) # Right border removed
-    ax.spines['bottom'].set_position('zero') # Sets the X-axis in the center
+    ax.spines['top'].set_visible(False)  # Top border removed
+    ax.spines['right'].set_visible(False)  # Right border removed
+    ax.spines['bottom'].set_position('zero')  # Sets the X-axis in the center
     ax.plot(x, y1, label=label1,color=color1)
     ax.plot(x, y2, label=label2,color=color2)
     ax.plot(x, y2-y1, dashes=[10, 5, 10, 5], label='delta', color='g')
     plt.xlabel('Stock Price')
     plt.ylabel('Profit and loss')
-    plt.legend()
+    plt.legend(loc='best')
     plt.show()
 
 class Graph:
@@ -70,3 +82,28 @@ class Graph:
         headers=['Settl.C', 'OIC', 'Day Vol C', 'Last C', 'bid C', 'ask C','C',
                  'strike', 'P', 'bid P', 'ask P', 'Last P', 'Day Vol P', 'OIP', 'Settl.P']
         display(HTML(tabulate.tabulate(page.data, tablefmt='html', headers=headers)))
+    def profit_rainbow(self, assets, direction, title=None):
+        """
+        :param assets: list of options or strategies to graph
+        :param direction: str 'long' or 'short'
+
+        >>> g.profit_rainbow(put.values,'short')
+        """
+        assets_color = [(key, value)
+                      for key, value
+                      in zip(assets, rainbow_color(len(assets)))]
+        fig, ax = plt.subplots(figsize=(12, 10))
+        plt.style.use('dark_background')
+        ax.spines['top'].set_visible(False)  # Top border removed
+        ax.spines['right'].set_visible(False)  # Right border removed
+        ax.spines['bottom'].set_position('zero')  # Sets the X-axis in the center
+        for asset, color in assets_color:
+            y = asset.payoff(self.sT, direction)
+            label=repr(asset)
+            ax.plot(self.sT, y, label=label,color=color)
+        plt.xlabel('Stock Price')
+        plt.ylabel('Profit and loss')
+        plt.legend(loc='best')
+        if title:
+            plt.title(title)
+        plt.show()
