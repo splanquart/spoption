771ceeaf4c2adc1191909457088665e52d26ca3d
Stéphane Planquart <stephane@planquart.com>
2018-11-22 08:03:00 +0100

    improve euronext interface

--- euronext.py
+++ euronext.py
@@ -1,16 +1,28 @@
+from enum import Enum
+
 import requests
 from bs4 import BeautifulSoup
 
 from product import Option
 
+class Ticker(Enum):
+    CACPXA = 'PXA-DPAR'
+    CAC1PX = '1PX-DPAR'
+    CAC2PX = '2PX-DPAR'
+    CAC4PX = '4PX-DPAR'
+    CAC5PX = '5PX-DPAR'
 
 class Page:
-    def __init__(self, site=None, ticker_idx=0, params=None):
+    """
+    liste des options euronext CAC40 :
+    https://derivatives.euronext.com/en/equity-index-derivatives/contract-list?quicktabs_309=4#quicktabs-309
+    """
+    def __init__(self, site=None, ticker=None, ticker_idx=0, params=None):
         self.tickers = ['PXA-DPAR', '4PX-DPAR', '2PX-DPAR', '1PX-DPAR', '5PX-DPAR']
         # liste des options euronext CAC40 :
         # https://derivatives.euronext.com/en/equity-index-derivatives/contract-list?quicktabs_309=4#quicktabs-309
         self.site = "https://derivatives.euronext.com/fr/products/index-options" if not site else site
-        self.ticker = self.tickers[ticker_idx]
+        self.ticker = ticker.value if ticker else self.tickers[ticker_idx]
         self.params = "Class_type=0&Class_symbol=&Class_exchange=&ex=&ps=999&md=11-2018" if not params else params
 
     def fetch(self):
@@ -22,7 +34,7 @@ class Page:
         self.put = {}
         return self.soup
 
-    def scrap_options(self):
+    def scrap_options(self, multiplier=1):
         div = self.soup.find("div", {"class": "call-put-table"})
         trs = div.find_all("tr")
         elmts = {}
@@ -32,18 +44,17 @@ class Page:
         for tr in trs[3:-1]:
             tds = tr.find_all('td')
             self.data.append([td.text for td in tds])
+            strike = float(tds[7].text)
             if (str(tds[4].text) != '-' and
-                str(tds[5].text) != '-' and
-                str(tds[9].text) != '-' and
-                str(tds[10].text) != '-') :
-                strike = float(tds[7].text)
+                str(tds[5].text) != '-'):
                 vente = float(tds[4].text)
                 achat = float(tds[5].text)
-                c = Option('Call', strike=strike, achat=achat, vente=vente)
+                c = Option('Call', strike=strike, achat=achat, vente=vente, multiplier=multiplier)
+                self.call[float(c.strike)] = c
+            if (str(tds[9].text) != '-' and
+                str(tds[10].text) != '-'):
                 vente = float(tds[9].text)
                 achat = float(tds[10].text)
-                p = Option('Put', strike=strike, achat=achat, vente=vente)
-                elmts[float(c.strike)] = {'call': c, 'put': p}
-                self.call[float(c.strike)] = c
+                p = Option('Put', strike=strike, achat=achat, vente=vente, multiplier=multiplier)
                 self.put[float(p.strike)] = p
-        return elmts
+        return (self.call, self.put)
